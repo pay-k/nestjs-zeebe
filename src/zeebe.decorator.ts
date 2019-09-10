de5cdbc36a076942788076c7ade97196ff712cb2
Josh Wulf <josh@magikcraft.io>
2019-09-10 17:10:37 +0200

    Rebase on zeebe-node 0.20.3

--- src/zeebe.decorator.ts
+++ src/zeebe.decorator.ts
@@ -1,6 +1,6 @@
 import { MessagePattern, PatternMetadata } from '@nestjs/microservices';
 import { ZeebeWorkerProperties } from './zeebe.interfaces';
-import { ZBWorkerOptions } from 'zeebe-node/dist/lib/interfaces';
+import { ZBWorkerOptions } from 'zeebe-node/interfaces';
 
 export const ZeebeWorker =  (type: string, options?: ZBWorkerOptions) => {
     return MessagePattern({ type, options: options || null });
